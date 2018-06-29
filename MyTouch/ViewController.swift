7673c639181054e29d3d07c7318847edd96061af
Tommy Lin <tommy60703@gmail.com>
2018-06-29 20:58:02 +0800

    New Xcode project

---
+++ MyTouch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTouch
+//
+//  Created by Tommy Lin on 2018/6/29.
+//  Copyright © 2018年 NTU HCI Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
