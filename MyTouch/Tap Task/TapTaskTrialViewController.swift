c8306b466648d446efeefef92381eea0dc43963c
Oliveck10 <oliveck10@gmail.com>
2018-07-09 13:35:46 +0800

    add swift 4.1 supplementary script

--- MyTouch/Tap Task/TapTaskTrialViewController.swift
+++ MyTouch/Tap Task/TapTaskTrialViewController.swift
@@ -75,6 +75,31 @@ extension TapTaskTrialViewController: TapTrialViewDataSource {
     }
 }
 
+// For swift 4.1
+extension MutableCollection {
+    /// Shuffles the contents of this collection.
+    mutating func shuffle() {
+        let c = count
+        guard c > 1 else { return }
+        
+        for (firstUnshuffled, unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
+            // Change `Int` in the next line to `IndexDistance` in < Swift 4.1
+            let d: Int = numericCast(arc4random_uniform(numericCast(unshuffledCount)))
+            let i = index(firstUnshuffled, offsetBy: d)
+            swapAt(firstUnshuffled, i)
+        }
+    }
+}
+
+extension Sequence {
+    /// Returns an array with the contents of this sequence, shuffled.
+    func shuffled() -> [Element] {
+        var result = Array(self)
+        result.shuffle()
+        return result
+    }
+}
+
 func positionGenerator(columns: Int, rows: Int, repeats: Int) -> [(Int, Int)] {
     return (0..<repeats).flatMap { _ in (0..<columns).flatMap { c in (0..<rows).map { r in (c, r) } } }
 }
