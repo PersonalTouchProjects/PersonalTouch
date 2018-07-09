36f2158abb00151fe632755f79177bc395b6e2d8
Oliveck10 <oliveck10@gmail.com>
2018-07-09 13:35:15 +0800

    fix RawTouch dependency (from UITouch)

--- MyTouch/Models/RawTouch.swift
+++ MyTouch/Models/RawTouch.swift
@@ -28,10 +28,10 @@ extension RawTouch {
        
         var rawValue: Int
         
-        static var force    = UITouch.Properties(rawValue: 1 << 0)
-        static var azimuth  = UITouch.Properties(rawValue: 1 << 1)
-        static var altitude = UITouch.Properties(rawValue: 1 << 2)
-        static var location = UITouch.Properties(rawValue: 1 << 3) // For predicted Touches
+        static var force    = RawTouch.Properties(rawValue: 1 << 0)
+        static var azimuth  = RawTouch.Properties(rawValue: 1 << 1)
+        static var altitude = RawTouch.Properties(rawValue: 1 << 2)
+        static var location = RawTouch.Properties(rawValue: 1 << 3) // For predicted Touches
     }
 }
 
