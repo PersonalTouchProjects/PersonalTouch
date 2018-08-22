366209b8b17b8a40510b3a63903347ee3d6b307c
Tommy Lin <tommy60703@gmail.com>
2018-08-22 15:07:59 +0800

    Remove drag and drop task from task selection view controller

--- MyTouch/Sources/Tasks/Task Selection/TaskCollectionDelegate.swift
+++ MyTouch/Sources/Tasks/Task Selection/TaskCollectionDelegate.swift
@@ -29,57 +29,53 @@ final class TaskCollectionDelegate: NSObject, UICollectionViewDataSource, UIColl
     // MARK: - UICollectionViewDataSource
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 8
+        return 7
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! TaskCollectionViewCell
         
-        switch indexPath.item {
-        case 0:
-            cell.taskTitleLabel.text = "點擊"
-            cell.subtitleLabel.text  = "點擊螢幕上的十字"
-            cell.isCompleted         = !tapTask.trials.isEmpty
-            
-        case 1:
-            cell.taskTitleLabel.text = "掃動"
-            cell.subtitleLabel.text  = "順著指定方向掃動"
-            cell.isCompleted         = !swipeTask.trials.isEmpty
-            
-        case 2:
-            cell.taskTitleLabel.text = "拖曳"
-            cell.subtitleLabel.text  = "將矩形拖曳至目標區域"
-            cell.isCompleted         = !dragAndDropTask.trials.isEmpty
-            
-        case 3:
-            cell.taskTitleLabel.text = "水平滾動"
-            cell.subtitleLabel.text  = "將矩形滾動至目標區域"
-            cell.isCompleted         = !horizontalScrollTask.trials.isEmpty
-            
-        case 4:
-            cell.taskTitleLabel.text = "垂直滾動"
-            cell.subtitleLabel.text  = "將矩形滾動至目標區域"
-            cell.isCompleted         = !verticalScrollTask.trials.isEmpty
-            
-        case 5:
-            cell.taskTitleLabel.text = "雙指縮放"
-            cell.subtitleLabel.text  = "將矩形放大或縮小至目標大小"
-            cell.isCompleted         = !pinchTask.trials.isEmpty
-            
-        case 6:
-            cell.taskTitleLabel.text = "雙指旋轉"
-            cell.subtitleLabel.text  = "將指北針旋轉至正北"
-            cell.isCompleted         = !rotationTask.trials.isEmpty
-            
-        case 7:
-            cell.taskTitleLabel.text = "長按"
-            cell.subtitleLabel.text  = "長按目標方塊"
-            cell.isCompleted         = !longPressTask.trials.isEmpty
-            
-        default:
-            break
-        }
+        let configuration: [() -> Void] = [
+            {
+                cell.taskTitleLabel.text = "點擊"
+                cell.subtitleLabel.text  = "點擊螢幕上的十字"
+                cell.isCompleted         = !self.tapTask.trials.isEmpty
+            },
+            {
+                cell.taskTitleLabel.text = "長按"
+                cell.subtitleLabel.text  = "長按目標方塊"
+                cell.isCompleted         = !self.longPressTask.trials.isEmpty
+            },
+            {
+                cell.taskTitleLabel.text = "掃動"
+                cell.subtitleLabel.text  = "順著指定方向掃動"
+                cell.isCompleted         = !self.swipeTask.trials.isEmpty
+            },
+            {
+                cell.taskTitleLabel.text = "水平滾動"
+                cell.subtitleLabel.text  = "將矩形滾動至目標區域"
+                cell.isCompleted         = !self.horizontalScrollTask.trials.isEmpty
+            },
+            {
+                cell.taskTitleLabel.text = "垂直滾動"
+                cell.subtitleLabel.text  = "將矩形滾動至目標區域"
+                cell.isCompleted         = !self.verticalScrollTask.trials.isEmpty
+            },
+            {
+                cell.taskTitleLabel.text = "雙指縮放"
+                cell.subtitleLabel.text  = "將矩形放大或縮小至目標大小"
+                cell.isCompleted         = !self.pinchTask.trials.isEmpty
+            },
+            {
+                cell.taskTitleLabel.text = "雙指旋轉"
+                cell.subtitleLabel.text  = "將指北針旋轉至正北"
+                cell.isCompleted         = !self.rotationTask.trials.isEmpty
+            }
+        ]
+        
+        let op = configuration[indexPath.item]
+        op()
         
         return cell
     }
@@ -91,52 +87,48 @@ final class TaskCollectionDelegate: NSObject, UICollectionViewDataSource, UIColl
         
         var taskViewController: UIViewController?
         
-        switch indexPath.item {
-        case 0:
-            let vc = TapTaskInstructionViewController()
-            vc.task = tapTask
-            taskViewController = vc
-            
-        case 1:
-            let vc = SwipeTaskInstructionViewController()
-            vc.task = swipeTask
-            taskViewController = vc
-            
-        case 2:
-            let vc = DragAndDropTaskInstructionViewController()
-            vc.task = dragAndDropTask
-            taskViewController = vc
-            
-        case 3:
-            let vc = ScrollTaskInstructionViewController()
-            vc.axis = .horizontal
-            vc.task = horizontalScrollTask
-            taskViewController = vc
-            
-        case 4:
-            let vc = ScrollTaskInstructionViewController()
-            vc.axis = .vertical
-            vc.task = verticalScrollTask
-            taskViewController = vc
-            
-        case 5:
-            let vc = PinchTaskInstructionViewController()
-            vc.task = pinchTask
-            taskViewController = vc
-            
-        case 6:
-            let vc = RotationTaskInstructionViewController()
-            vc.task = rotationTask
-            taskViewController = vc
-            
-        case 7:
-            let vc = LongPressTaskInstructionViewController()
-            vc.task = longPressTask
-            taskViewController = vc
-            
-        default:
-            break
-        }
+        let configuration: [() -> Void] = [
+            {
+                let vc = TapTaskInstructionViewController()
+                vc.task = self.tapTask
+                taskViewController = vc
+            },
+            {
+                let vc = LongPressTaskInstructionViewController()
+                vc.task = self.longPressTask
+                taskViewController = vc
+            },
+            {
+                let vc = SwipeTaskInstructionViewController()
+                vc.task = self.swipeTask
+                taskViewController = vc
+            },
+            {
+                let vc = ScrollTaskInstructionViewController()
+                vc.axis = .horizontal
+                vc.task = self.horizontalScrollTask
+                taskViewController = vc
+            },
+            {
+                let vc = ScrollTaskInstructionViewController()
+                vc.axis = .vertical
+                vc.task = self.verticalScrollTask
+                taskViewController = vc
+            },
+            {
+                let vc = PinchTaskInstructionViewController()
+                vc.task = self.pinchTask
+                taskViewController = vc
+            },
+            {
+                let vc = RotationTaskInstructionViewController()
+                vc.task = self.rotationTask
+                taskViewController = vc
+            }
+        ]
+        
+        let op = configuration[indexPath.row]
+        op()
         
         if let taskViewController = taskViewController, let viewController = viewController {
             let navController = UINavigationController(rootViewController: taskViewController)
@@ -150,13 +142,12 @@ final class TaskCollectionDelegate: NSObject, UICollectionViewDataSource, UIColl
         
         switch indexPath.item {
         case 0: return tapTask.trials.isEmpty
-        case 1: return swipeTask.trials.isEmpty
-        case 2: return dragAndDropTask.trials.isEmpty
+        case 1: return longPressTask.trials.isEmpty
+        case 2: return swipeTask.trials.isEmpty
         case 3: return horizontalScrollTask.trials.isEmpty
         case 4: return verticalScrollTask.trials.isEmpty
         case 5: return pinchTask.trials.isEmpty
         case 6: return rotationTask.trials.isEmpty
-        case 7: return longPressTask.trials.isEmpty
         default:
             return true
         }
